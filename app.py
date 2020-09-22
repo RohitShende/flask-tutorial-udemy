61f755e35d08b5b77223077b4c74da52c6134b00
rshende <rshende@vmware.com>
2020-09-22 16:02:21 +0530

    added git commit hooks

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 """ This is a flask application """
-
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def home():
     """
-        Home page rest api
-        Returns: None
+    Home page rest api
+    Returns: None
 
     """
-    return 'Hello World'
+    return "Hello World"
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
